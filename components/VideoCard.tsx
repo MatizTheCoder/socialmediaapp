ba5121edc7556406837ccfba0e92491ed27166ee
Emre KARAMAN <matizdrum@gmail.com>
2022-09-30 22:13:57 +0300

    update with bugs needn to be fixed

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import { Video } from '../types'
+import React, { useState, useEffect, useRef } from 'react'
 import { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs'
+import { GoVerified } from 'react-icons/go'
+import { Video } from '../types'
 
 interface IProps {
   post:Video
 }
 
-const VideoCart: NextPage<IProps> = ({ post }) => {
+const VideoCard: NextPage<IProps> = ({ post }) => {
   return (
-    <div>VideoCart</div>
+    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+      <div>
+        <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+          <div className='md:w-16 md:h-16 w-10 h-10'>
+            <Link href="/">
+            <>
+            <Image
+                  width={62}
+                  height={62}
+                  className='rounded-full'
+                  src={post.postedBy.image}
+                  alt='user-profile'
+                  layout='responsive'
+                />
+            </>
+            </Link>             
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default VideoCart
+export default VideoCard
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // images:{
+  //   domains: ['yt3.gghpt.com'],
+  // }
 }
 
 module.exports = nextConfig
