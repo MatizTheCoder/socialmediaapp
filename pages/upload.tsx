1578958a647e4b950fa0eea8fd97c223ac0a58ea
Emre KARAMAN <matizdrum@gmail.com>
2022-10-02 10:11:49 +0300

    imports

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { FaCloudDownloadAlt } from 'react-icons/fa'
+import { MdDelete } from 'react-icons/fa'
+import axios from 'axios'
+
+import userAuthStore from '../store/authStore'
+import { client } from '../utils/client'
 
 const Upload = () => {
   return (
