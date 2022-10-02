bfd13845d385fbafd9052b93af5ecc54f720ea0e
Emre KARAMAN <matizdrum@gmail.com>
2022-10-02 10:56:07 +0300

    styles

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { FaCloudDownloadAlt } from 'react-icons/fa'
-import { MdDelete } from 'react-icons/fa'
+import { FaCloudUploadAlt } from 'react-icons/fa'
+import { MdDelete } from 'react-icons/md'
 import axios from 'axios'
 import { SanityAssetDocument } from '@sanity/client'
 
@@ -34,17 +34,17 @@ const Upload = () => {
   }
 
   return (
-    <div>
-      <div>
+    <div className='flex w-full h-full absolute left-0 top-[60px] lg:top-[70px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center'>
+      <div className=' bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6'>
         <div>
           <div>
-            <p>Upload Video</p>
-            <p>Post a video to your account</p>
+            <p className='text-2xl font-bold'>Upload Video</p>
+            <p className='text-md text-gray-400 mt-1'>Post a video to your account</p>
           </div>
-          <div>
+          <div className=' border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center  outline-none mt-10 w-[260px] h-[458px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100'>
             {isLoading ? (
-              <p>Uploading...</p>
-            ): (
+              <p className='text-center text-3xl text-red-400 font-semibold'>Uploading...</p>
+            ) : (
               <div>
                 {videoAsset ? (
                   <div>
@@ -52,30 +52,29 @@ const Upload = () => {
                     src={videoAsset.url}
                     loop
                     controls
-                    className='rounded-xl h[450px mt-16 bg-black'
+                    className='rounded-xl h[450px] mt-16 bg-black'
                     >
-
                     </video>
                   </div>
                 ) : (
                   <div>
-                    <label >
-                      <div>
-                          <div>
-                            <p>
-                              <FaCloudDownloadAlt  className='textgray-300 text-6xl'/>
+                    <label className='cursor-pointer'>
+                      <div className='flex flex-col items-center justify-center'>
+                          <div className='flex flex-col items-center justify-center'>
+                            <p className='font-bold text-xl'>
+                              <FaCloudUploadAlt  className='text-gray-300 text-6xl'/>
                             </p>
                             <p>
                               Upload video
                             </p>
                           </div>
-                          <p>
+                          <p className='text-gray-400 text-center mt-10 text-sm leading-10'>
                             MP4 or WebM or ogg <br />
                             720x1280 or higher <br />
                             Up to 10 minutes <br />
                             Less than 2GB
                           </p>
-                          <p>
+                          <p className='bg-[#F51997] text-center mt-8 rounded text-white text-md font-medium p-2 w-52 outline-none'>
                             Select File
                           </p>
                       </div>
@@ -83,6 +82,7 @@ const Upload = () => {
                       type="file"
                       name='upload-video' 
                       onChange={uploadVideo}
+                      className='w-0 h-0'
                       />
                     </label>
                   </div>
